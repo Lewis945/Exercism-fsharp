9f93dbecdce22f012b3d08c4b1775928b12256ff
Vitalii Zakharov <lewiss945@gmail.com>
2021-07-13 18:56:47 +0300

    Etl task.

---
+++ etl/Etl.fs
@@ -0,0 +1,8 @@
+﻿module Etl
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = 
+    scoresWithLetters
+    |> Map.toSeq
+    |> Seq.collect (fun (k,v) -> v |> List.map (fun c -> (c |> System.Char.ToLowerInvariant, k)))
+    |> Seq.distinctBy fst
+    |> Map.ofSeq
---
+++ pangram/Pangram.fs
@@ -0,0 +1,7 @@
+﻿module Pangram
+
+let isPangram (input: string): bool = 
+    input.ToLowerInvariant()
+    |> Seq.filter System.Char.IsLetter
+    |> Seq.distinct
+    |> Seq.length = 26
