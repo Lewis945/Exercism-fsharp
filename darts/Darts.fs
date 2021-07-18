6c866cecefd3c4677ac009965c6c5f0ce53ceaa9
Vitalii Zakharov <lewiss945@gmail.com>
2021-07-18 22:24:25 +0300

    Darts solution.

---
+++ darts/Darts.fs
@@ -0,0 +1,10 @@
+﻿module Darts
+
+let score (x: double) (y: double): int = 
+    pown x 2 + pown y 2
+    |> sqrt
+    |> function
+       | d when d > 10. -> 0
+       | d when d > 5. -> 1
+       | d when d > 1. -> 5
+       | _ -> 10
