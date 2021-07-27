cefed34f22a4d287c2c81ce1a862c90a14f689aa
Vitalii Zakharov <lewiss945@gmail.com>
2021-07-27 11:48:46 +0300

    Merge proverb solution.

---
+++ proverb/Proverb.fs
@@ -0,0 +1,12 @@
+﻿module Proverb
+
+let recite1 (input: string list): string list = 
+    let rec recite' acc items =
+        match items with
+        | [] -> acc []
+        | _::[] -> recite' (acc >> (fun a -> $"And all for the want of a {input.Head}." :: a)) []
+        | x::y::ws -> recite' (acc >> (fun a -> $"For want of a {x} the {y} was lost." :: a)) (y::ws)
+
+    input
+    |> recite' id
+    |> List.rev
