4f4d0a3ee60a64b24329ce400b45f46dbc8669f4
Vitalii Zakharov <lewiss945@gmail.com>
2021-07-31 18:40:00 +0300

    Merge isogram solution.

---
+++ isogram/Isogram.fs
@@ -0,0 +1,8 @@
+﻿module Isogram
+
+let isIsogram str = 
+    str
+    |> Seq.map System.Char.ToLower
+    |> Seq.filter System.Char.IsLetter
+    |> Seq.countBy id
+    |> Seq.forall (fun (_, c) -> c = 1)
