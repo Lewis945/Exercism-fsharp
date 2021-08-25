ca65ae5dc99ebfe8058540979608b256c26ef9fd
Vitalii Zakharov <lewiss945@gmail.com>
2021-08-25 20:54:15 +0300

    My solution.

--- perfect-numbers/PerfectNumbers.fs
+++ perfect-numbers/PerfectNumbers.fs
@@ -2,4 +2,20 @@
 
 type Classification = Perfect | Abundant | Deficient 
 
-let classify n : Classification option = failwith "You need to implement this function."
+let getFactors n =
+    [1..n-1]
+    |> List.filter (fun x -> n % x = 0)
+
+let validate n =
+    if n <= 0 then None else Some n
+
+let classify n : Classification option = 
+    n
+    |> validate
+    |> Option.map getFactors
+    |> Option.map List.sum
+    |> Option.map (function
+                    | s when s = n -> Perfect
+                    | s when s > n -> Abundant
+                    | _ -> Deficient)
+    
